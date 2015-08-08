dfc453ea3e0d6c0e0fbc3d965f9e8a32a7ea470e
actepotentiel <ruefbenjamin@gmail.com>
2015-08-08 21:53:16 +0200

    titre

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -3,7 +3,7 @@
 // Init the application configuration module for AngularJS application
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
-	var applicationModuleName = 'SandBox';
+	var applicationModuleName = 'Easic';
 
 	var applicationModuleVendorDependencies = ['ngResource', 'ui.router', 'ui.bootstrap', 'ui.utils', 'angularFileUpload'];
 
