86e5c0cdb25cc4fb164321b7c6d2d926628c9e33
jeansaigne <aymerictibere@gmail.com>
2015-08-11 20:16:25 +0200

    Ajout du forwarding des paramètres de la requête vers les adapters

--- modules/requests/server/controllers/api-adapters.server.controller.js
+++ modules/requests/server/controllers/api-adapters.server.controller.js
@@ -6,14 +6,13 @@
 /**
  * Youtube api adapter
  */
-exports.youtubeApi = function(callback) {
+exports.youtubeApi = function(params, callback) {
     var YouTube = require('youtube-node');
-
     var youTube = new YouTube();
 
     youTube.setKey('AIzaSyBVsIBrr7VmuhNN-NvRVWw-gZA4vjj1YeA');
 
-    youTube.search('mc25cm', 1, function(error, result) {
+    youTube.search(params.q, 1, function(error, result) {
         if (error) {
             callback({message: 'getYoutubeMedias: Une erreur s\'est produite.'}, null);
         }
@@ -26,12 +25,12 @@ exports.youtubeApi = function(callback) {
 /*
 * Soundcloud api adapter
 * */
-exports.soundcloudApi = function(callback) {
+exports.soundcloudApi = function(params, callback) {
     var SC = require('node-soundcloud');
     SC.init({
         id: '297a1ba0221212502262213f257f0e7f'
     });
-    SC.get('/tracks', { q: 'toto', limit: 1 }, function(err, tracks) {
+    SC.get('/tracks', { q: params.q, limit: 1 }, function(err, tracks) {
         if (err)
             callback(err,null);
         else
@@ -40,24 +39,25 @@ exports.soundcloudApi = function(callback) {
 };
 
 /*
-* Dailymotion api adapter
+* TODO Dailymotion api adapter
 * */
-exports.dailymotionApi = function(callback) {
+exports.dailymotionApi = function(params, callback) {
  callback(null, {});
 };
 
 /*
 * Vimeo api adapter
 * */
-exports.vimeoApi = function(callback) {
+exports.vimeoApi = function(params, callback) {
     var Vimeo = require('vimeo').Vimeo;
+    console.log(params);
     var lib = new Vimeo('fdfd207f8a4d8e18f98a5355f81906130e60fd35', 'gNP2iwWTv5nm+q68KWIog1bQKBGA+k3n7X9pelhhpjmYvRWw+Ks75LCezMMW/dSrubwp8yJcMxy2CFoID4z5Rua03DgFRyRTGBPanPyv25jedEBUJSmqSCs5y4i5y6k4', '3362e455675da4faa4a27a67554df2e4');
     lib.request({
         // This is the path for the videos contained within the staff picks channels
         path : '/videos',
         // This adds the parameters to request page two, and 10 items per page
         query : {
-            query: 'toto',
+            query: params.q,
             per_page : 1
         }
     }, function(err, results) {
@@ -68,9 +68,9 @@ exports.vimeoApi = function(callback) {
 /*
 * Deezer api adapter
 * */
-exports.deezerApi = function(callback) {
+exports.deezerApi = function(params, callback) {
     var deezer = require('node-deezer-api-client');
-    deezer.requestData('/search?q=track:\'toto\'&limit=1', function(err, results) {
+    deezer.requestData('/search?q=track:\''+params.q+'\'&limit=1', function(err, results) {
         callback(err, {deezer: results});
     });
 };
@@ -78,9 +78,9 @@ exports.deezerApi = function(callback) {
 /*
 * Spotify api adapter
 * */
-exports.spotifyApi = function(callback) {
+exports.spotifyApi = function(params, callback) {
     var spotify = require('spotify');
-    spotify.search({ type: 'track', query: 'toto', limit: 1 }, function(err, data) {
+    spotify.search({ type: 'track', params: params.q, limit: 1 }, function(err, data) {
         callback(err, {spotify: data});
     });
 };
