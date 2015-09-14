117fc333c42e3245a7005e21a18d5f2d47142e85
actepotentiel <ruefbenjamin@gmail.com>
2015-09-14 19:41:42 +0200

    prod

--- config/assets/production.js
+++ config/assets/production.js
@@ -8,14 +8,17 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
 			],
 			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.min.js',
-				'public/lib/angular-animate/angular-animate.min.js',
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-				'public/lib/angular-file-upload/angular-file-upload.min.js',
-				'public/lib/angular-material/angular-material.min.js'
+				'public/lib/angular/angular.js',
+				'public/lib/angular-aria/angular-aria.min.js',
+				'public/lib/angular-resource/angular-resource.js',
+				'public/lib/angular-animate/angular-animate.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
+				'public/lib/angular-material/angular-material.min.js',
+				'public/lib/jquery/dist/jquery.js',
+				'public/custom-lib/custom-lib.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
